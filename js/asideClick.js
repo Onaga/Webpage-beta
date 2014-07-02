7c7260bba6570425e13b0909196578a3dc9b18c9
dimitarstoyanov90 <dimitarstoyanov90@gmail.com>
2014-07-02 20:13:28 +0300

    aside updated

--- js/asideClick.js
+++ js/asideClick.js
@@ -2,13 +2,18 @@ $( document ).ready(function() {
  $("#showMe").click(function(){
   $('#showMe').css('visibility', 'hidden')
   $('#hideMe').css('visibility', 'visible')
-  $('#aside').css('height', '450px')
-
+  $('#aside').css('height', '493px')
+  $('#main').css('width', '67%')
+  $('.img').css('width', '300px', 'height' , '200px')
+  $('#aside').css('overflow', 'scroll')
+  
 });
  $("#hideMe").click(function(){
   $('#hideMe').css('visibility', 'hidden')
   $('#showMe').css('visibility', 'visible')
    $('#aside').css('height', '40px')
-
+   $('#main').css('width', '100%')
+   $('.img').css('width', '460px', 'height' , '200px')
+   $('#aside').css('overflow', 'hidden')
 });
 });
